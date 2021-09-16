45baa8400ba072c6844d464c4c6daf0c170aaa53
Julia Romualdo <75337060+romualdojulia@users.noreply.github.com>
2021-09-16 10:47:02 -0300

    Add files via upload

---
+++ Imersões Alura/Va mais fundo/exercicios/aula-01/main.js
@@ -0,0 +1,19 @@
+var nome = 'Julia'
+var notaPrimeiroBimestre = 9
+var notaSegundoBimestre = 7
+var notaTerceiroBimestre = 4
+var notaQuartoBimestre = 2
+
+var notaFinal =
+  (notaPrimeiroBimestre +
+    notaSegundoBimestre +
+    notaTerceiroBimestre +
+    notaQuartoBimestre) /
+  4
+
+var notaFixada = notaFinal.toFixed(1)
+
+console.log('Bem vindo ' + nome)
+console.log(notaFixada)
+
+// isso é um comentário //
