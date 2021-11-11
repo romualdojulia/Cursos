4d13111edea6360d4dba070276ccc3164a972a91
Julia Romualdo <75337060+romualdojulia@users.noreply.github.com>
2021-11-11 13:59:32 -0300

    Add files via upload

---
+++ Curso em Video/MySQL/aula-02.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE CADASTRO
+DEFAULT CHARACTER SET utf8
+DEFAULT COLLATE utf8_general_ci;
+
+create table pessoas (
+id int not null auto_increment,
+nome varchar(30) not null,
+nascimento date,
+sexo enum ('M', 'F'),
+peso decimal (5,2),
+altura decimal (3,2), 
+nacionalidade varchar(20) default 'Brasil', 
+primary key (id)
+) default charset = utf8
