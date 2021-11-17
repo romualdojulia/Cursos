c1da30d05499f1ad5887ee5d8268a2cfb3c1f47e
Julia Romualdo <75337060+romualdojulia@users.noreply.github.com>
2021-11-17 19:34:12 -0300

    Add files via upload

---
+++ Curso em Video/MySQL/aula-06-1.sql
@@ -0,0 +1,18 @@
+desc gafanhotos;
+
+alter table pessoas
+rename to gafanhotos;
+
+alter table pessoas
+add column profissao varchar(10) after nome;
+
+alter table pessoas
+modify column profissao varchar(20) not null default '';
+
+alter table pessoas
+change column profissao prof varchar(20);
+
+alter table pessoas
+drop column profissao;
+
+select * from pessoas;
---
+++ Curso em Video/MySQL/aula-06-3.sql
@@ -0,0 +1,13 @@
+create table if not exists teste(
+	id int,
+    nome varchar(10),
+    idade int
+);
+
+insert into teste values
+('1', 'Pedro', '22'),
+('2', 'Maria', '12');
+
+select * from teste;
+
+drop table if exists teste;
